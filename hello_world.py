50a04401f9c8619f7c29e5e53d5860d3ea1832c3
FlyinDutch19 <ji@yuyi.nl>
2022-06-12 21:41:20 +0200

    change the name of file

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,1 @@
-new_list = ['e', 'a', 'b', 'd', 'c', 3, 23.235]
-print(new_list)
-new_list.sort()
-my_sorted_list = new_list
-print(my_sorted_list)
-new_list.reverse()
-my_reverse_list = new_list
-print(my_reverse_list)
+print("Hello")
