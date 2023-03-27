590e56592b3211e62acacba4fcf8307467cf5de7
Ji Zhang <ji.zhang@westcoastsupplygroup.com>
2023-03-27 20:34:53 +0200

    setup git

--- if_else.py
+++
@@ -1,8 +0,0 @@
-import random as r
-hungry = r.randint(0,2)
-if hungry == 2:
-	print("I'm hungry!")
-elif hungry == 1:
-	print("I'm not so hungry, but I want to eat something.")
-else:
-	print("I'm not hungry.'")
